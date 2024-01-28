63815b16d4d1a718df76ab5a292957de0c2094ea
Den <den_m1985@mail.ru>
2024-01-28 14:00:02 +0200

    lesson 6

---
+++ SpringLesson_6/src/main/java/com/example/books_store/model/Book.java
@@ -0,0 +1,28 @@
+package com.example.books_store.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+//@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private int publicationYear;
+
+    public Book(String title, String author, String isbn, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+    }
+
+}
---
+++ SpringLesson_6/src/main/java/com/example/books_store/service/BookService.java
@@ -0,0 +1,34 @@
+package com.example.books_store.service;
+
+import com.example.books_store.model.Book;
+import com.example.books_store.repositiry.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+    @Autowired
+    private final BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Optional<Book> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+}
